54bbe6027da1064ce207c247b65a60d9667706a4
Gautam2503 <gautamnaggn357@gmail.com>
2024-04-08 01:52:02 +0530

    shit done.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import unraw from "unraw";
+import Card from "./Card";
+import Markdown from 'react-markdown'
 import { useState } from "react";
 import illustration from "../assets/images/illustration-intro.svg";
 
@@ -91,18 +94,42 @@ const json = [
   { Company: "Wipro Ltd. (WIPRO.NS)", Symbol: "WIPRO.NS" },
 ];
 
-// return <li>{this.props.label + " - " + this.props.value}</li>;
-
 const Hero = () => {
   const [ticker, setTicker] = useState("");
+  const [summary, setSummary] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [cardData, setcardData] = useState([]);
 
   const handleChange = (event) => {
     setTicker(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`First Name: ${ticker}, Last Name: ${ticker}`);
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`https://unveil-backend.onrender.com/summary/${ticker}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch summary");
+      }
+      const data = await response.text();
+      const cleanedData = data.replace(/^"(.*)"$/, '$1');
+      //console.log(cleanedData);
+      setSummary(unraw(cleanedData));
+    } catch (error) {
+      console.error(error);
+    }
+    try {
+      const response = await fetch(`https://unveil-backend.onrender.com/news/${ticker}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch summary");
+      }
+      const data = await response.json();
+      console.log(data);
+      setcardData(data);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
   };
 
   return (
@@ -116,47 +143,48 @@ const Hero = () => {
             "Unveil" in this context suggests the action of revealing or bringing to light the
             hidden or underlying narrative associated with a particular stock.
           </p>
-
-          {/* <div className="flex justify-center md:justify-start">
-            <a
-              href="#"
-              className="p-3 bg-brightRed text-white rounded-full 
-                baseline pt-2 px-6 hover:bg-brightRedLight "
-            >
-              Get Started
-            </a>
-          </div> */}
         </div>
         <div className=" right md:w-1/2">
           <img src={illustration} alt="illustration" />
         </div>
       </div>
-      
-        {/* <div className="collapse-title text-xl font-medium"></div> */}
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col py-12 justify-center items-center "
+      <form className="flex flex-col py-12 justify-center items-center">
+        <select
+          onChange={handleChange}
+          value={ticker}
+          className="select select-info w-full max-w-xl"
+          disabled={loading}
         >
-          <select
-            onChange={handleChange}
-            value={ticker}
-            className="select select-info w-full max-w-xl"
-          >
-            <option disabled selected>
-              Stock Options
+          <option disabled selected>
+            Stock Options
+          </option>
+          {json.map((item, i) => (
+            <option key={i} value={item.Symbol}>
+              {item.Company}
             </option>
-            {json.map((item, i) => (
-              <option key={i} value={item.Symbol}>
-                {item.Company}
-              </option>
-            ))}
-            {/* <option>English</option>
-          <option>Japanese</option>
-          <option>Italian</option> */}
-          </select>
-          <div className="collapse bg-base-200 my-6">
-          <input type="checkbox" />
-          <button type="submit" value="submit" className="btn btn-ghost btn-circle collapse-title ">
+          ))}
+        </select>
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className="btn btn-ghost btn-circle collapse-title"
+          disabled={loading}
+        >
+          {loading ? (
+            <svg
+              className="animate-spin h-5 w-5 mr-3 border-t-2 border-b-2 border-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.001 8.001 0 0112 4.472v3.518a4 4 0 00-4 4h3.518zm10.142-.707A8.004 8.004 0 0112 20.472v3.518a9.956 9.956 0 006.131-2.336l-2.959-2.959zM20.472 12h3.518a8.003 8.003 0 01-2.337 6.13l-2.96-2.96A9.969 9.969 0 0020.472 12z"
+              ></path>
+            </svg>
+          ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -171,13 +199,20 @@ const Hero = () => {
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
             </svg>
-          </button>
+          )}
+        </button>
+        <div className="collapse bg-base-200 my-6">
+          <input type="checkbox" />
           <div className="collapse-content">
-          <p>hello</p>
+          {summary ? <Markdown>{summary}</Markdown> : <p>No summary available</p>}
+          <div className="flex align-items-center justify-center py-6 gap-5 w-[90%]">
+              {cardData.map((item, i) => (
+                  <Card key={i} card={item} />
+              ))}
+          </div>
+          </div>
         </div>
-      </div>
-        </form>
-
+      </form>
     </>
   );
 };
