5c084b5155947611a8d190ea6bffec50dcf4c03a
Debjit <debjitmukhopadhyay316@gmail.com>
2024-04-07 13:48:50 +0530

    first changes

---
+++ src/Tickertape.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+function Tickertape() {
+  return (
+    <>
+      <div className="ticker-tape">
+        <div className="ticker">
+          <div className="ticker-item">Company updates</div>
+          <div className="ticker-item">Company updates</div>
+          <div className="ticker-item">Company updates</div>
+          <div className="ticker-item">Company updates</div>
+          <div className="ticker-item">Company updates</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Tickertape;
